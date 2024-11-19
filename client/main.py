d423b1ed002d1b4a691fcdbe4ca6773d8d097c7d
u1F98E <u1f98e@proton.me>
2024-11-19 13:02:46 -0800

    Improve cli and add temporary keys to test token validation

--- client/main.py
+++ client/main.py
@@ -1,14 +1,17 @@
+import argparse
 import datetime
 import itertools
+import sched
 import socket
 import threading
 import json
-from typing import List
+import time
+from typing import List, Tuple
 import jwt
 import sys
 from wireguard_tools import WireguardKey
 
-from zt_host.auth import ZTAuth, PeerAuthToken
+from zt_host.auth import AUTH_PUBLIC_KEY, ZTAuth
 from zt_host.wireguard import ZTDevice, Peer
 
 MAX_MESSAGE_SIZE=4096
@@ -23,16 +26,20 @@ class Session:
 class ZtContext:
     wg: ZTDevice
     auth: ZTAuth
+    scheduler: sched.scheduler
+
     name: str
     pub_key: str
 
     incoming_sessions: List[Session] = []
     outgoing_sessions: List[Session] = []
 
-    def __init__(self, _wg: ZTDevice):
+    def __init__(self, _wg: ZTDevice, _auth: ZTAuth):
         self.wg = _wg
+        self.auth = _auth
+        self.scheduler = sched.scheduler(time.time, time.sleep)
 
-    def expire_incoming(self):
+    def expire_incoming_sessions(self):
         def f(session: Session):
             if session.expiration < now:
                self.wg.remove_peer(session.peer_pub_key) 
@@ -42,25 +49,11 @@ class ZtContext:
 
         now = datetime.datetime.now()
         self.incoming_sessions[:] = itertools.filterfalse(f, self.incoming_sessions)
-
+    
     def refresh_session(self, peer_addr: str):
         for session in self.outgoing_sessions:
             if session.peer_addr == peer_addr:
                 token = self.auth.request_refresh_token()
-
-    def send_connect_request(peer_addr: str, token: PeerAuthToken):
-        # Open a socket to the peer so we know the laddr,
-        # the local address used to communicate with it
-        peer_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        peer_sock.bind((peer_addr, COMM_PORT))
-        local_addr, _ = peer_sock.getsockname()
-
-        request = {
-            "type": "connect",
-            "token": token.jwt
-        }
-
-        peer_sock.sendall(json.dumps(request) + '\n')
 
     def request_connection(self, peer_addr: str):
         """Authorize and request a connection to a peer"""
@@ -72,38 +65,54 @@ class ZtContext:
 
         token = self.auth.request_connection_token(peer_addr, local_addr)
 
-        # TODO: This is a fake token for now
-        token.jwt = { 
-            "name": self.name, 
-            "wg_pub": self.pub_key,
-            "addr": local_addr,
-            "ips": [local_addr],
-        }
-        print(token.jwt)
-
         request = {
             "type": "connect",
             "token": token.jwt
         }
 
-        peer_sock.sendall(json.dumps(request) + '\n')
+        s = peer_sock.makefile('rw')
+        s.write(json.dumps(request) + '\n')
+        response = json.load(s.readline(MAX_MESSAGE_SIZE))
+        if "result" not in response:
+            raise Exception("Response message from peer did not contain 'result' field")
+        if response["result"] != "ok":
+            if "msg" in response:
+                raise Exception(f"Peer returned an error during connection request: {response["msg"]}")
+            else:
+                raise Exception(f"Peer returned an error during connection request")
+
+        session = Session()
+        session.peer_addr = peer_addr
+        session.peer_pub_key = response['pub_key']
+        session.expiration = token.expiration
+        self.outgoing_sessions.append(session)
 
 def handle_connect_request(token: str, client_addr: str, ctx: ZtContext):
-    # decoded = jwt.decode(token, key=TOKEN_PUBLIC_KEY, algorithms=['RS256',])
+    decoded = jwt.decode(token, key=AUTH_PUBLIC_KEY, algorithms=['RS256',])
     decoded = json.load(token)
 
     peer = Peer()
     peer.name = decoded["name"]
-    peer.public_key = decoded["wg_pub"]
+    peer.public_key = decoded["pub"]
     peer.endpoint_addr = decoded["addr"]
     peer.endpoint_port = WIREGUARD_PORT
     peer.allowed_ips = decoded["ips"]
+    expiration = decoded["exp"]
 
     if client_addr != peer.endpoint_addr:
         print("Error: Failed to add new peer, connection address does not match endpoint address")
         return
 
     ctx.wg.add_peer(peer)
+
+    session = Session()
+    session.expiration = expiration
+    session.peer_pub_key = peer.public_key
+    session.peer_addr = peer.endpoint_addr
+    ctx.incoming_sessions.append(session)
+
+    # Schedule an expiration check when this session is supposed to expire
+    ctx.scheduler.enterabs(expiration, 1, ZtContext.expire_incoming_sessions, argument=[ctx])
 
 def process_peer_message(data: str, client_addr: str, ctx: ZtContext):
     msg = json.load(data)
@@ -135,35 +144,46 @@ def listen_for_peers(host, port, ctx: ZtContext):
         except Exception as e:
             print(f"Error while processing message: {e}")
 
-def daemon_main():
+def daemon_main(args):
     private_key = WireguardKey.generate()
     wg = (
         ZTDevice.create("wgTest0", 
                         private_key = str(private_key),
                         public_key = str(private_key.public_key()),
-                        wg_addr = '10.0.0.2',
+                        wg_addr = args.listen_addr, # TODO: Get from auth server or something
                         listen_port = WIREGUARD_PORT)
     )
-
-    ctx = ZtContext(wg)
+    auth = ZTAuth((args.auth_addr, int(args.auth_port)))
+    ctx = ZtContext(wg, auth)
 
     try:
         peer_listen_args = ("localhost", COMM_PORT, ctx)
         # threading.Thread(target=listen_for_peers, args=peer_listen_args)
         listen_for_peers(*peer_listen_args)
-        print("Hi")
     finally:
         wg.down()
 
+def connect_subcommand(args):
+    print("todo")
+
 def _help():
-    print("zt-host [connect <peer-ip>]")
+    print("zt-host <auth address>")
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "--help" or sys.argv[1] == "help":
-            _help()
-        if sys.argv[1] == "connect":
-            peer_ip = sys.argv[2]
-            request_connection(peer_ip)
-    
-    daemon_main()
+    parser = argparse.ArgumentParser(
+        prog="zt-host",
+    )
+
+    subcommands = parser.add_subparsers(required=True)
+    listen_sub = subcommands.add_parser("listen")
+    listen_sub.add_argument("listen_addr", help="The address for this host's wireguard tunnel")
+    listen_sub.add_argument("auth_addr", help="The address for the root auth node")
+    listen_sub.add_argument("auth_port", default="8080", nargs="?", help="The port for the root auth node")
+    listen_sub.set_defaults(func=daemon_main)
+
+    connect_sub = subcommands.add_parser("connect")
+    connect_sub.set_defaults(func=connect_subcommand)
+
+    args = parser.parse_args(sys.argv[1:])
+    if hasattr(args, "func"):
+        args.func(args)
--- client/zt_host/auth.py
+++ client/zt_host/auth.py
@@ -1,6 +1,13 @@
 import datetime
 import socket
-from typing import Any, Callable, List
+from typing import Any, Callable, List, Tuple
+
+import jwt
+
+AUTH_PUBLIC_KEY="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0ntFvw4YEisJ67WyOJWYsmEPmLjCDqg+OhlUAvMy4DcB+fYM9/EvCupWpx2MTGFf4St88nrEW9pfGDqI4zuMrCaKda7bchY8DpCUK13ONuuQv0noeXQgMqlIL+Xp15GMOOqjaRsQ+NHio3zX3hLPP7+IuawESEhnT4aq9CGYIDYqUbKpyIZL3RmQp6vP/ZFOrE8ccSnsa2shembY/UZXTmsZae4+bPKYH7K9XhvAibO1S9T4z0o661r7BwtbbzsldZhsZUBn5mXIoi4eGl7RGaqYG7W0M320K5OC9BNP22aBpNUq0CyOh3uOfSpulyQp1g+XP9iHTqwEtMM5lRFqgwIDAQAB"
+
+# TODO: Remove when authentication works
+FAKE_AUTH_PRIVATE_KEY="MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDSe0W/DhgSKwnrtbI4lZiyYQ+YuMIOqD46GVQC8zLgNwH59gz38S8K6lanHYxMYV/hK3zyesRb2l8YOojjO4ysJop1rttyFjwOkJQrXc4265C/Seh5dCAyqUgv5enXkYw46qNpGxD40eKjfNfeEs8/v4i5rARISGdPhqr0IZggNipRsqnIhkvdGZCnq8/9kU6sTxxxKexrayF6Ztj9RldOaxlp7j5s8pgfsr1eG8CJs7VL1PjPSjrrWvsHC1tvOyV1mGxlQGfmZciiLh4aXtEZqpgbtbQzfbQrk4L0E0/bZoGk1SrQLI6He459Km6XJCnWD5c/2IdOrAS0wzmVEWqDAgMBAAECggEAKtB+gH2Kce+lRfggt5fehrJtrMAwYTDh74mFaFA2Ehu+YC0nuCusSQkf9UBycHsCONhSwzQryw9hdpk5jRdo5v+z/HpEEzHop4HLUgLz1oIWYILVMhdoEUYT9AJNjdcCcfVwpgmmayaudlkHxBmJZMs+MZ6HBHkN4sRe8+f9fNeJVhbaYwbxyrffAU/cPQml3hUEryBKwmVQmGGPZXbAtrt03yVVRrWK7H97AG7qzTblNNggC9OxLksTHFx73+UPfkYZNS+j67AKtrZK/tzk9+O49M9nsd+/cxDAt83ddjEFRe5Zln1eXDl8h48Bctzg6CSiDNtCjcnH+XM2TuxFOQKBgQDpa0YFPP8V4ZHWzoKE80EUYtxE4nI5R41jH9tg325rwSfax+eLufRpZ2y7axeGiVscz/QANiqFYt3efiSiUHjYdIMM+rlxGbOZ2djPapENKsBqioHonsRLvDfnIUo9hTsUbos7v6TIbV7TKnKSVP9sU/NZOuZRa7k+g9GL2ruHLQKBgQDm1/EAm7XSJpcwowKqIyVAnCjgCuf6rbnGnfITuI/iiT7zYfDMUM9BrDmmd1RH5u04uMQrC9mKNe22U3w8aAB5TMp9VFmpSd0/iFj2LOpwcPJXppaCkH2y0C1j7k/V8OHA66Ik5QkcgXe402iqY8gRu9gujRx4HjQW4RfIYSnGbwKBgDMcCBLxjjEuWQ6d3TpbQS7DjtOOedBM+Ipx2UlW4wi5HI7tIqgXqYrC2K4Y/ue4f7jJV/YL5jeatFYZbNAVqsBKkr9uztgS9p6DctPH08b8S3GIrnVO/lBPADadtXHPEGai5d3JUr2IJTE7pCaiceM4ZpptKS0+1yr3FhT3agRpAoGAGFX5jUPdFiuFUtZMiJ0t5zLPSFc9/3pjFGJAS/3wBUTMwyG3HgMC5nx+k2MPGoloxssLuzIDnAN1bPw+I6wfKKqEylOjJCqqTXFVV5aCnJn7omvogBk1dy1lK5zLA9XiJtFwhY0zubKYaX64EQZ16BCyiK3Nq4gbm7HdLP25RrcCgYAaWTEb/siTK3GPSLsQEqZtAk6aWiwGEp8HIekXVUlIOPg5eoqcFvY69GG9uYTLSnpLc9ALcdHIRt7qTQhm8z+F4T/hOKgiffwA/Q4dUMLGdBpaJhxia1QirnVVjL6Zo1Zfo/LZgg2pJcJhPy6qeIEi2jPBBvgMt6E5m87ZPAksXQ=="
 
 class AuthNode:
 	addr: str
@@ -30,9 +37,8 @@ class ZTAuth:
 	nodes: List[AuthNode] # In order of rank
 	system_cred: str # TODO
 
-	def __init__(self, root_addr: str, root_port: int):
-		self.root_addr = root_addr
-		self.root_port = root_port
+	def __init__(self, root_addr: Tuple[str, int]):
+		self.root_addr, self.root_port = root_addr
 		self.refresh_cache_list()
 
 	def refresh_cache_list(self):
@@ -43,7 +49,7 @@ class ZTAuth:
 
 		root_node = AuthNode(self.root_addr, self.root_port)
 		self.nodes.append(root_node)
-		self.nodes.sort(key = AuthNode.rank)
+		self.nodes.sort(key = lambda node: node.rank)
 
 	def on_best_node(self, func: Callable[[socket.socket], Any]):
 		sock = None
@@ -66,8 +72,21 @@ class ZTAuth:
 
 	def request_connection_token(self, peer_addr: str, local_addr: str) -> PeerAuthToken:
 		def request(sock: socket.socket) -> PeerAuthToken:
-			# TODO
-			return PeerAuthToken()	
+			token = PeerAuthToken()
+			# TODO: This is a fake token for now
+			claims = { 
+				"name": self.name, 
+				"pub": self.pub_key,
+				"addr": local_addr,
+				"ips": [local_addr],
+				"exp": datetime.datetime.fromisoformat("2030-01-01").isoformat()
+			}
+
+			token.jwt = jwt.encode(token, key=AUTH_PUBLIC_KEY, algorithms=['RS256',])
+			token.expiration = claims['exp']
+			print(token.jwt)
+
+			return token
 
 		return self.on_best_node(request)
 
